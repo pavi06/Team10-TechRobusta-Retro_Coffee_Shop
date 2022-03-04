2b44fa5b783710202869b6b8de508c1f8f05f325
SwethaSri-S <sswethasri2003@gmail.com>
2022-03-04 11:22:17 -0800

    edited

--- RetroCoffeeShop/src/com/retro/shop/Report.java
+++ RetroCoffeeShop/src/com/retro/shop/Report.java
@@ -1,1 +1,28 @@
-ls;jdjf
+import java.sql.*;
+import java.io.*;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.*;
+import org.jfree.data.jdbc.JDBCCategoryDataset;
+
+public class Report {
+	public static void main(String[] args) throws Exception {
+
+	       String query = "SELECT * from chart";
+	       JDBCCategoryDataset dataset = new JDBCCategoryDataset(
+		"jdbc:mysql://localhost:3306/test", 
+                "com.mysql.jdbc.Driver","root", "root");
+	       dataset.executeQuery(query);
+	       JFreeChart chart =  
+                ChartFactory.createBarChart3D("Id", "Name", "Sales",
+	        dataset, PlotOrientation.VERTICAL, true, true, false);
+	       try {
+	       ChartUtilities.saveChartAsJPEG(new File("C:/chart.jpg"),
+                chart,400, 300);
+	       } catch (IOException e) {
+	       System.out.println("Problem in creating chart.");
+  }
+	}
+}
